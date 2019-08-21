ae3cfd17b12430de7b6bd87fe0a5e7dcf6078d0d
Daler Abdullaev <dalerleo@gmail.com>
2019-08-21 12:05:23 +0500

    Updates

---
+++ precache-manifest.08535bacbda23b2fd4c8d76fbf01dbf4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e093016343fda9232ee666f21ffe99d",
+    "url": "/DalerLeo/create-app/index.html"
+  },
+  {
+    "revision": "28b82c7f33df662d8dcd",
+    "url": "/DalerLeo/create-app/static/css/main.52128000.chunk.css"
+  },
+  {
+    "revision": "a3d1133816645ff98e72",
+    "url": "/DalerLeo/create-app/static/js/2.27919ba9.chunk.js"
+  },
+  {
+    "revision": "28b82c7f33df662d8dcd",
+    "url": "/DalerLeo/create-app/static/js/main.00bcdde5.chunk.js"
+  },
+  {
+    "revision": "c29586c9e03b4d61c7dc",
+    "url": "/DalerLeo/create-app/static/js/runtime~main.3eb8742f.js"
+  }
+]);
